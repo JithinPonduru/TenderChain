679d707d13f9196c3de9bf7c823c5f607f0b26e6
JithinPonduru <jithin_ponduru@srmap.edu.in>
2024-03-12 01:00:23 +0530

    Windows

--- tendercontract/src/components/Home_page.js
+++ tendercontract/src/components/Home_page.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import '/home/Jithin/Documents/3rd_Year/2nd_Sem/Software Engineering /tendercontract_backend/tendercontract/src';
 import Body from './Home_Body.js';
 import Footer from './Home_Footer.js';
 import Header from './Home_Header.js';
