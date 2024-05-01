a382640557f049cd050e7ce1d80b9656e31cb8be
JithinPonduru <jithin_ponduru@srmap.edu.in>
2024-05-01 09:49:54 +0530

    final

--- tendercontract/src/components/Home_Header.js
+++ tendercontract/src/components/Home_Header.js
@@ -4,7 +4,7 @@ function Header() {
   return (
     <div id='Header1'>
 
-      <meta charset="utf-8" />
+      <meta charSet ="utf-8" />
       <meta content="width=device-width, initial-scale=1.0" name="viewport" />
 
       <title>TenderChain </title>
@@ -14,7 +14,7 @@ function Header() {
       <link href="assets/img/favicon.png" rel="icon" />
       <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon" />
       <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+      <link rel="preconnect" href="https://fonts.gstatic.com" />
       <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,600;1,700&family=Roboto:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=Work+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet" />
       <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
       <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet" />
